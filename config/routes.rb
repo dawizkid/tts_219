396c8bf3368886e7492fc62cb59ae5e4dc5f1e40
TammyLLC <me@TammyLLC.com>
2014-02-19 09:44:12 -0500

    Changed color and fonts to Big Caslon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Macmal::Application.routes.draw do
   resources :blogs
+root 'blog#index'
+  
+  "blog#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
